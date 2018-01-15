6539f593a41b6e29f0bb1bc00d25947328413722
Administrator <Administrator@WIN7-20170112VU>
2018-01-15 14:22:52 +0900

    commit 2

--- openRef/src/main/java/com/openref/app/HomeController.java
+++ openRef/src/main/java/com/openref/app/HomeController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.openref.domain.UserVO;
+import com.openref.login.KakaoLogin;
+
 /**
  * Handles requests for the application home page.
  */
@@ -27,54 +31,16 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	
-	// home 무시 
-	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
 	@RequestMapping(value ="/main.do" ,method = RequestMethod.POST)
-	public String main(@RequestBody JSONObject data) {
+	public String main(@RequestParam()) {
 		
-		String name =data.get("id").toString();
-		System.out.println(name);
 		
 		return "main";
 	}
-	@RequestMapping(value = "/login.do")
-	public String login() {
+	@RequestMapping(value ="/login.do")
+	public String login(){
 		
 		return "login";
 	}
-	
-	@RequestMapping(value = "/loginCheck.do", method = RequestMethod.GET)
-	public String loginCheck(HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("=============로그인 체크=================");
-		
-		String returnURL ="";
-		String request_nickname = request.getParameter("name");
-		
-		if(request_nickname!=null) { // 카카오톡이나 네이버에서 세션 값이 넘어오면, 메인으로
-			
-			returnURL ="main";
-			
-		}else { // 없으면 로그인 페이지로
-			
-			returnURL ="login";
-		}
-		
-		return returnURL;
-	}
-	
-	
-	
+
 }
